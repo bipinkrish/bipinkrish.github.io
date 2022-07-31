b06718bf49d4c4b098f31b9ed2d5fac95ba90a89
Bipin <87369440+bipinkrish@users.noreply.github.com>
2022-07-31 11:10:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,24 +1,111 @@
-### Hi there 👋, I'm Bipin Krishna 🇮🇳
+<div align = "center">
 
-# Info
+<h1><a href="https://2kabhishek.github.io/projects">Projects</a></h1>
 
-- 🌱 I’m currently learning Python.
+<a href="https://github.com/2KAbhishek/projects/blob/main/LICENSE">
+<img alt="License" src="https://img.shields.io/github/license/2kabhishek/projects?style=flat&color=eee&label="> </a>
 
+<a href="https://github.com/2KAbhishek/projects/graphs/contributors">
+<img alt="People" src="https://img.shields.io/github/contributors/2kabhishek/projects?style=flat&color=ffaaf2&label=People"> </a>
 
-<img align="left" alt="brijeshkrishna's GitHub Stats" src="https://github-readme-stats.vercel.app/api?username=bipinkrish&theme=nightowl&show_icons=true&count_private=true&hide=prs" />
+<a href="https://github.com/2KAbhishek/projects/stargazers">
+<img alt="Stars" src="https://img.shields.io/github/stars/2kabhishek/projects?style=flat&color=98c379&label=Stars"></a>
 
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
+<a href="https://github.com/2KAbhishek/projects/network/members">
+<img alt="Forks" src="https://img.shields.io/github/forks/2kabhishek/projects?style=flat&color=66a8e0&label=Forks"> </a>
 
-[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=bipinkrish)](https://github.com/bipinkrish)
+<a href="https://github.com/2KAbhishek/projects/watchers">
+<img alt="Watches" src="https://img.shields.io/github/watchers/2kabhishek/projects?style=flat&color=f5d08b&label=Watches"> </a>
 
-[python]: https://python.org
-[vs]: https://vscode.com
-[none]: https://brijeshkrishna.github.io/home
+<a href="https://github.com/2KAbhishek/projects/pulse">
+<img alt="Last Updated" src="https://img.shields.io/github/last-commit/2kabhishek/projects?style=flat&color=e06c75&label="> </a>
+
+<h3>Showcase All Your Projects 🛍️🎇</h3>
+
+<figure>
+  <img src= "images/screenshot.png" alt="projects screenshot" style="width:100%">
+  <br/>
+  <figcaption>projects screenshot</figcaption>
+</figure>
+
+</div>
+
+## Introducing Projects
+
+Present all your projects in style with a super customizable web app! ✨
+
+`Projects` uses the GitHub API to list all your GitHub projects in a nice searchable grid.
+Also shows pretty programming icons using `devicons`.
+
+## Inspiration
+
+Needed a way to display all my projects, used my [portfolio's project section](https://2kabhishek.github.io/#projects) as inspiration.
+
+## Getting Projects
+
+To get projects, follow these steps:
+
+```bash
+git clone https://github.com/2kabhishek/projects
+cd projects
+```
+
+### Setup Your Own Projects
+
+You can easily set up projects to show your own repos.
+
+- Fork the repo
+- Clone it
+- Open up `script.js` and update the `username` variable to your GitHub username.
+- Open up `index.html` and update the `<title></title>` tag to make it your username
+- Push your changes
+- Go to repo settings on GitHub and enable GitHub Pages.
+
+The site should be live on `https://<your-username>.github.io/projects`
+
+Here's the projects page for [@sindresorhus](github.com/sindresorhus)
+
+![sindresorhus's projects](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/n78j6j4itdbad2cg2h0h.png) 
+
+#### Number Of Repos
+
+The number of repos is controlled by the `maxPages` variable, the GitHub API supports 100 repos per page max.
+If you have less than 100 repos, set `maxPages` to 1, if you have 300 then 3.
+
+You can also edit the fetch query to reduce the per page repo count.
+> There's no pagination, all repos are shown on the same page.
+
+#### Authenticated Requests
+
+If you are working locally and notice the API is not sending over data, it might be because of rate limit on GitHub API requests.
+
+You can either wait for an hour or setup a personal access token on GitHub and pass that into the fetch request in `script.js`
+
+## Viewing projects
+
+Open `index.html` in your favorite browser or visit [2kabhishek.github.io/projects](https://2kabhishek.github.io/projects).
+
+## How it was built
+
+Projects was built using `HTML` `CSS` & `JavaScript`.
+It was built on Neovim and the python http server.
+Uses GitHub API for data and Devicons for programming icons.
+
+## What I learned
+
+- Learned about a few quirks of the fetch API, especially implementation of `maxPages`.
+- Flex, box-shadow and some other CSS tricks were revisited.
+
+## What's next
+
+Add more languages to devicons if necessary.
+
+Hit the ⭐ button if you found this useful.
+
+## More Info
+
+<div align="center">
+
+<a href="https://github.com/2KAbhishek/projects">Source</a> | <a href="https://2kabhishek.github.io/projects">Website</a>
+
+</div>
---
+++ script.js
@@ -0,0 +1,128 @@
+const username = 'bipinkrish';
+const maxPages = 2;
+const repoList = document.querySelector('.repo-list');
+const reposSection = document.querySelector('.repos');
+const filterInput = document.querySelector('.filter-repos');
+
+let devicons = {
+    'C#': '<i class="devicon-csharp-plain colored"></i> C#',
+    'C++': '<i class="devicon-cplusplus-plain colored"></i> C++',
+    C: '<i class="devicon-c-plain colored"></i> C',
+    CSS: '<i class="devicon-css3-plain colored"></i> CSS',
+    Dockerfile: '<i class="devicon-docker-plain colored"></i> Docker',
+    Elixir: '<i class="devicon-elixir-plain colored"></i> Elixir',
+    Go: '<i class="devicon-go-plain colored"></i> Go',
+    HTML: '<i class="devicon-html5-plain colored"></i> HTML',
+    Haskell: '<i class="devicon-haskell-plain colored"></i> Haskell',
+    Java: '<i class="devicon-java-plain colored" style="color: #ffca2c"></i> Java',
+    JavaScript: '<i class="devicon-javascript-plain colored"></i> JavaScript',
+    'Jupyter Notebook': '<i class="devicon-jupyter-plain colored"></i> Jupyter',
+    Kotlin: '<i class="devicon-kotlin-plain colored" style="color: #796bdc"></i> Kotlin',
+    Lua: '<i class="devicon-lua-plain-wordmark colored" style="color: #0000d0"></i> Lua',
+    Nim: '<i class="devicon-nixos-plain colored" style="color: #FFC200"></i> Nim',
+    PHP: '<i class="devicon-php-plain colored"></i> PHP',
+    PLSQL: '<i class="devicon-sqlite-plain colored"></i> PLSQL',
+    Processing:
+        '<i class="devicon-processing-plain colored" style="color: #0096D8"></i> Processing',
+    Python: '<i class="devicon-python-plain colored" style="color: #3472a6"></i> Python',
+    Ruby: '<i class="devicon-ruby-plain colored"></i> Ruby',
+    Rust: '<i class="devicon-rust-plain colored" style="color: #DEA584"></i> Rust',
+    Sass: '<i class="devicon-sass-original colored"></i> Sass',
+    Scala: '<i class="devicon-scala-plain colored"></i> Scala',
+    Shell: '<i class="devicon-bash-plain colored" style="color: #89E051"></i> Shell',
+    Swift: '<i class="devicon-swift-plain colored"></i> Swift',
+    TypeScript: '<i class="devicon-typescript-plain colored"></i> TypeScript',
+    null: '<i class="devicon-markdown-original"></i> Markdown'
+};
+
+// get information from github profile
+const getProfile = async function () {
+    const res = await fetch(
+        `https://api.github.com/users/${username}`
+        // {
+        //     headers: {
+        //         Accept: 'application/vnd.github+json',
+        //         Authorization: 'token your-personal-access-token-here'
+        //     }
+        // }
+    );
+    const profile = await res.json();
+    displayProfile(profile);
+};
+getProfile();
+
+// display infomation from github profile
+const displayProfile = function (profile) {
+    const userInfo = document.querySelector('.user-info');
+    userInfo.innerHTML = `
+        <figure>
+            <img alt="user avatar" src=${profile.avatar_url} />
+        </figure>
+        <div>
+            <h2><a href=${profile.blog}><strong>${profile.name}</strong></a></h2>
+            <p>${profile.bio}</p>
+            <p>
+                <strong>Location:</strong> ${profile.location}
+            </p>
+            <p>
+                <strong>@${profile.login} </strong>
+                Repos: ${profile.public_repos}
+                Gists: ${profile.public_gists}
+            </p>
+        </div>
+    `;
+};
+
+// get list of user's public repos
+const getRepos = async function () {
+    let repos = [];
+    let res;
+    for (let i = 1; i <= maxPages; i++) {
+        res = await fetch(
+            `https://api.github.com/users/${username}/repos?&sort=pushed&per_page=100&page=${i}`
+            // {
+            //     headers: {
+            //         Accept: 'application/vnd.github+json',
+            //         Authorization:
+            //             'token your-personal-access-token-here'
+            //     }
+            // }
+        );
+        let data = await res.json();
+        repos = repos.concat(data);
+    }
+    displayRepos(repos);
+};
+getRepos();
+
+// display list of all user's public repos
+const displayRepos = function (repos) {
+    filterInput.classList.remove('hide');
+    for (const repo of repos) {
+        let listItem = document.createElement('li');
+        listItem.classList.add('repo');
+        listItem.innerHTML = `
+            <h3>${repo.name}</h3>
+            <span>${repo.description}</span> <br/><br/>
+            <span>${devicons[repo.language]}</span> <br />
+            <br />
+            <a href=${repo.html_url}>View Project</a>`;
+        repoList.append(listItem);
+    }
+};
+
+// dynamic search
+filterInput.addEventListener('input', function (e) {
+    const search = e.target.value;
+    const repos = document.querySelectorAll('.repo');
+    const searchLowerText = search.toLowerCase();
+
+    for (const repo of repos) {
+        const lowerText = repo.innerText.toLowerCase();
+        if (lowerText.includes(searchLowerText)) {
+            repo.classList.remove('hide');
+        } else {
+            repo.classList.add('hide');
+        }
+    }
+});
